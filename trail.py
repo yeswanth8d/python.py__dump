95e1790f412b85de14b24eae1b8e2a12c8e3cf8c
yeswanth8d <yeswanth.8d@gmail.com>
2026-04-10 22:03:23 +0530

    nothinng

--- trail.py
+++ trail.py
@@ -2,13 +2,13 @@ a=input("enter ur name :")
 print(f"hello {a} ",end ="")
 print(69)
 
+
 #functions
 def fun_function():
-	a=int(input("num = "))
+	a=int(input("num= "))
 	return a
 
 fun_function()
-
 
 names=["bob", "jai", "ria"]
 print(type(names))
@@ -18,7 +18,5 @@ age=int(input("whats ur age"))
 print(f"age is {age}")
 
 
-
 z=10
 z1=20
-
